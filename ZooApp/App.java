fe09c69250b57a74d485c537fa949b0733f47579
Hale <786133@cognizant.com>
2019-11-06 16:56:58 -0700

    Built out create zoo step and List all zoos step.

--- ZooApp/App.java
+++ ZooApp/App.java
@@ -1,5 +1,7 @@
 package ZooApp;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
@@ -10,17 +12,59 @@ public class App {
         System.out.println("4 - Exit");
     }
 
+    static List<String> getNewZooInfo(){
+
+        Scanner input = new Scanner(System.in);
+
+        String[] questions = {"What is the name of your zoo?", 
+                            "What is the street address of your zoo?",
+                            "What is the city of your zoo?",
+                            "What is the zip of your zoo?",
+                            "What is the phone of your zoo?",
+                            "What is the email of your zoo?"};
+
+        List<String> answers = new ArrayList<String>();
+
+        for(String i : questions){
+            System.out.println(i);
+            answers.add(input.nextLine());
+        }
+       return answers;
+    }
+
+    static Zoo createZoo(){
+        List<String> info = getNewZooInfo();
+        String name = info.get(0);
+        String street = info.get(1);
+        String city = info.get(2);
+        String zip = info.get(3);
+        String phone = info.get(4);
+        String email = info.get(5);
+        Zoo newZoo = new Zoo(name, street, city, zip, phone, email);
+        return newZoo;
+    }
+
    public static void main(String[] args) {
+
     System.out.println("Welcome to the Zoo App!");
        Scanner input = new Scanner(System.in);
        Integer selection;
+       Zoo activeZoo;
+       List<Zoo> zooList = new ArrayList<>();
+
        do {
            mainMenu();
            selection = input.nextInt();
            switch (selection) {
                 case 1:
-                    Zoo zoo1 = new Zoo();
-                    zoo1.hello();
+                    zooList.add(createZoo());
+                    System.out.println("Zoo Created!");
+                    break;
+
+                case 2:
+                    for (Zoo zoo : zooList) {
+                        System.out.println(zoo.name);
+                    }
                     break;
 
                 case 4:
--- ZooApp/Zoo.java
+++ ZooApp/Zoo.java
@@ -2,8 +2,30 @@ package ZooApp;
 
 class Zoo {
 
-    public void hello(){
-       System.out.println("Hello from the Zoo Class!!!"); 
+    String name;
+    String street;
+    String city;
+    String zip;
+    String phone;
+    String email;
+    String[] animals;
+
+    public Zoo(String name, String street, String city, String zip, String phone, String email){
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String[] getInfo(){
+        String[] info = {this.name, this.street, this.city, this.zip, this.phone, this.email};
+        return info;
+    }
+
+    public String[] getAnimals(){
+        return this.animals;
     }
 
 }
